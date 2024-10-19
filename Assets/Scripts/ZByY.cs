c44003ebf3376b4c6504652873819a689e3a2a8f
Diogo de Andrade <diogo.g.andrade@gmail.com>
2024-10-19 11:01:24 +0100

    Added Z sorting and shadow on characters

---
+++ Assets/Scripts/ZByY.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ZByY : MonoBehaviour
+{
+    [SerializeField] private float baseY = 0.0f;
+    [SerializeField] private float slopeZ = 0.001f;
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.z = (pos.y - baseY) * slopeZ;
+        transform.position = pos;
+    }
+}
